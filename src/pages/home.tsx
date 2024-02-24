f6a28a37b3a8319f09eabf3ee80bec93594f31a8
guicoelhodev <guicoelhodev>
2024-02-24 00:57:20 -0300

    feat: add Project component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,15 @@
 import { CarouselTools } from '../components/Home/CarouselTools';
 import { Header } from '../components/Home/Header';
+import { Projects } from '../components/Home/Projects';
 
 export default function Home() {
 
   return (
-    <section class='bg-neutral-900'>
+    <section class='bg-neutral-900 flex flex-col'>
       <Header />
       <CarouselTools />
+
+      <Projects />
     </section>
   );
 }
